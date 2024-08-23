aa839d961b77f40b11fd13c5854b849bd6e0d4ca
Satvik <satvikchandra100@gmail.com>
2024-08-24 00:00:13 +0530

    add search bar (desktop)

--- components/property-items.tsx
+++ components/property-items.tsx
@@ -10,51 +10,74 @@ import {
 } from "@/components/ui/card";
 import { propertyState } from "@/state/atoms/property";
 import { MapPin } from "lucide-react";
-import { useRecoilValue } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { Input } from "./ui/input";
+import { useMediaQuery } from "usehooks-ts";
+import { useState } from "react";
 
 export default function PropertyItems() {
+  const isDesktop = useMediaQuery("(min-width: 768px)", {
+    initializeWithValue: false,
+  });
+
   const propertyListings = useRecoilValue(propertyState);
+  const setPropertyListings = useSetRecoilState(propertyState);
+
+  const [query, setQuery] = useState("");
 
   return propertyListings.items.length < 1 ? (
     <div className="w-full text-center">No Content to show</div>
   ) : (
-    <main className="grid lg:grid-cols-3 grid-rows-2 md:grid-cols-2 gap-4">
-      {propertyListings.items.map((item) => (
-        <Card key={item.id} className="flex flex-col justify-between shadow-lg">
-          <CardHeader>
-            <img
-              src="https://rapidapi.com/blog/wp-content/uploads/2018/10/architecture-1836070_640.jpg"
-              alt="property_image"
-              className="rounded-xl"
-            />
-            <CardTitle className="pt-2 text-xl">{item.title}</CardTitle>
-            <CardDescription className="flex items-center gap-1">
-              <MapPin className="w-4 h-4" />
-              {item.location}
-            </CardDescription>
-            <div className="flex items-center justify-between">
-              <span className="font-bold text-sm">Rs. {item.price}</span>
-              <span className="font-bold text-sm">
-                {item.bedrooms} bedroom(s)
-              </span>
-            </div>
-          </CardHeader>
-          <CardContent>
-            <div className="flex gap-2 mb-2">
-              <Badge>{item.type}</Badge>
-              {item.amenities?.map((amenity, index) => (
-                <Badge variant="outline" key={index}>
-                  {amenity}
-                </Badge>
-              ))}
-            </div>
-            <CardDescription className=" text-wrap">
-              {item.description}
-            </CardDescription>
-            <Button className="w-full mt-4">Book Now</Button>
-          </CardContent>
-        </Card>
-      ))}
-    </main>
+    <div className="flex flex-1 flex-col gap-2">
+      {isDesktop && (
+        <Input
+          placeholder="Search for properties..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      )}
+      <main className="grid lg:grid-cols-3 grid-rows-2 md:grid-cols-2 gap-4">
+        {propertyListings.items
+          .filter((val) => val.title.toLocaleLowerCase().includes(query))
+          .map((item) => (
+            <Card
+              key={item.id}
+              className="flex flex-col justify-between shadow-lg"
+            >
+              <CardHeader>
+                <img
+                  src="https://rapidapi.com/blog/wp-content/uploads/2018/10/architecture-1836070_640.jpg"
+                  alt="property_image"
+                  className="rounded-xl"
+                />
+                <CardTitle className="pt-2 text-xl">{item.title}</CardTitle>
+                <CardDescription className="flex items-center gap-1">
+                  <MapPin className="w-4 h-4" />
+                  {item.location}
+                </CardDescription>
+                <div className="flex items-center justify-between">
+                  <span className="font-bold text-sm">Rs. {item.price}</span>
+                  <span className="font-bold text-sm">
+                    {item.bedrooms} bedroom(s)
+                  </span>
+                </div>
+              </CardHeader>
+              <CardContent>
+                <div className="flex gap-2 mb-2">
+                  <Badge>{item.type}</Badge>
+                  {item.amenities?.map((amenity, index) => (
+                    <Badge variant="outline" key={index}>
+                      {amenity}
+                    </Badge>
+                  ))}
+                </div>
+                <CardDescription className=" text-wrap">
+                  {item.description}
+                </CardDescription>
+                <Button className="w-full mt-4">Book Now</Button>
+              </CardContent>
+            </Card>
+          ))}
+      </main>
+    </div>
   );
 }
